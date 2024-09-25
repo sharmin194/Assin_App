59c483b67cc7788beb40dd79794f67cf0257b996
sharmin194 <sharmin.ext01@gmail.com>
2024-09-26 01:47:47 +0600

    Updating CheckBox

--- app/src/main/java/com/sharmin/assignment_demoapp/MainActivity.java
+++ app/src/main/java/com/sharmin/assignment_demoapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sharmin.assignment_demoapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CheckBox milkCheckBox,sugarCheckBox,waterCheckBox;
+    private Button showButton;
+    private TextView resultTextView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        milkCheckBox = (CheckBox) findViewById(R.id.milkCheckBoxID);
+        sugarCheckBox = (CheckBox) findViewById(R.id.SugarCheckBoxID);
+        waterCheckBox = (CheckBox) findViewById(R.id.WaterCheckBoxID);
+
+        showButton = (Button) findViewById(R.id.ShowButtonId);
+        resultTextView = (TextView) findViewById(R.id.resultTextViewId);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                if(milkCheckBox.isChecked()){
+                    String value = milkCheckBox.getText().toString();
+                    stringBuilder.append(value + " is ordered "+"\n");
+                }
+                if(sugarCheckBox.isChecked()){
+                    String value = sugarCheckBox.getText().toString();
+                    stringBuilder.append(value + " is ordered "+"\n");
+                }
+                if(waterCheckBox.isChecked()){
+                    String value = waterCheckBox.getText().toString();
+                    stringBuilder.append(value + " is ordered "+"\n");
+                }
+                resultTextView.setText(stringBuilder);
+
+            }
         });
     }
 }
