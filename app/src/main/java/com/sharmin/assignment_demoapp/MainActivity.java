42ec949e3df9b18d131afab90e9f3da04e862a88
sharmin194 <sharmin.ext01@gmail.com>
2024-09-27 08:01:45 +0600

    making a minor changes in layout

--- app/src/main/java/com/sharmin/assignment_demoapp/MainActivity.java
+++ app/src/main/java/com/sharmin/assignment_demoapp/MainActivity.java
@@ -4,55 +4,122 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private CheckBox milkCheckBox,sugarCheckBox,waterCheckBox;
-    private Button showButton;
-    private TextView resultTextView;
-
-
+    private CheckBox milkCheckBox, sugarCheckBox, waterCheckBox;
+    private Button showCheckBoxButton, showRadioButton;
+    private TextView checkBoxResultTextView, radioResultTextView;
+    private RadioGroup radioGroup;
+    private RadioButton courseButton;
+    private RatingBar ratingBar;
+    private SeekBar seekBar;
+    private Switch notificationSwitch;
+    private TextView seekBarValueTextView, switchResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        milkCheckBox = (CheckBox) findViewById(R.id.milkCheckBoxID);
-        sugarCheckBox = (CheckBox) findViewById(R.id.SugarCheckBoxID);
-        waterCheckBox = (CheckBox) findViewById(R.id.WaterCheckBoxID);
+        // CheckBoxes er id initialize
+        milkCheckBox = findViewById(R.id.milkCheckBoxID);
+        sugarCheckBox = findViewById(R.id.SugarCheckBoxID);
+        waterCheckBox = findViewById(R.id.WaterCheckBoxID);
 
-        showButton = (Button) findViewById(R.id.ShowButtonId);
-        resultTextView = (TextView) findViewById(R.id.resultTextViewId);
+        // Buttons er id initialize
+        showCheckBoxButton = findViewById(R.id.ShowCheckBoxButtonId);
+        showRadioButton = findViewById(R.id.ShowRadioButtonId);
 
-        showButton.setOnClickListener(new View.OnClickListener() {
+        // TextViews er id
+        checkBoxResultTextView = findViewById(R.id.checkBoxResultTextViewId);
+        radioResultTextView = findViewById(R.id.radioResultTextViewId);
+        seekBarValueTextView = findViewById(R.id.seekBarValueId);
+        switchResultTextView = findViewById(R.id.switchResultTextViewId);
+
+        //  RadioGroup er id initialize
+        radioGroup = findViewById(R.id.radioGroupId);
+
+        // RatingBar er id initialize
+        ratingBar = findViewById(R.id.ratingBarId);
+
+        // SeekBar
+        seekBar = findViewById(R.id.seekBarId);
+
+        //  Switch
+        notificationSwitch = findViewById(R.id.switchId);
+
+        // CheckBox Button Click
+        showCheckBoxButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                StringBuilder stringBuilder = new StringBuilder();
-
-                if(milkCheckBox.isChecked()){
-                    String value = milkCheckBox.getText().toString();
-                    stringBuilder.append(value + " is ordered "+"\n");
+                StringBuilder result = new StringBuilder();
+                if (milkCheckBox.isChecked()) {
+                    result.append("Milk is ordered.\n");
                 }
-                if(sugarCheckBox.isChecked()){
-                    String value = sugarCheckBox.getText().toString();
-                    stringBuilder.append(value + " is ordered "+"\n");
+                if (sugarCheckBox.isChecked()) {
+                    result.append("Sugar is ordered.\n");
                 }
-                if(waterCheckBox.isChecked()){
-                    String value = waterCheckBox.getText().toString();
-                    stringBuilder.append(value + " is ordered "+"\n");
+                if (waterCheckBox.isChecked()) {
+                    result.append("Water is ordered.\n");
                 }
-                resultTextView.setText(stringBuilder);
+                checkBoxResultTextView.setText(result.toString());
+            }
+        });
 
+        // RadioButton Click portion
+        showRadioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                if (selectedId != -1) {
+                    courseButton = findViewById(selectedId);
+                    String language = courseButton.getText().toString();
+                    radioResultTextView.setText("You prefer: " + language);
+                } else {
+                    radioResultTextView.setText("Please select a language.");
+                }
+            }
+        });
+
+        // RatingBar Change Listener
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                radioResultTextView.setText("Rating: " + rating);
+            }
+        });
+
+        // SeekBar Change Listener
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarValueTextView.setText("Brightness: " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        // Switch Listener
+        notificationSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                switchResultTextView.setText("Notifications Enabled");
+            } else {
+                switchResultTextView.setText("Notifications Disabled");
             }
         });
     }
